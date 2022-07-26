9b6754db9292c2efc9c1e3ddd4a3e65f959dbc03
Luoxi Pan <l.panpax@gmail.com>
2022-07-26 20:59:49 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const testHeading = document.createElement('h1');
+testHeading.textContent = 'Todo List';
+document.body.appendChild(testHeading);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todo List',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(txt)$/i,
+                type: 'asset/resource',
+            }
+        
+        ],
+    },
+};
