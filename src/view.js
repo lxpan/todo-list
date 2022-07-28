8231820cdc4e13194ad67288bbdbdf25fb70a136
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 21:06:50 +1000

    Fix: use querySelector instead of getElementById

--- src/view.js
+++ src/view.js
@@ -8,7 +8,7 @@ export default (function view() {
     }
 
     function insertProjectItems(query, textToUpdate) {
-        const contentDiv = document.getElementById(query);
+        const contentDiv = document.querySelector(query);
         const newItem = document.createElement('div');
         newItem.textContent = textToUpdate;
         contentDiv.appendChild(newItem);
