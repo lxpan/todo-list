554e7eeef06ae3c60febc127e23d7594900b9e44
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 21:14:23 +1000

    Refactor: generalise variable name

--- src/view.js
+++ src/view.js
@@ -1,17 +1,17 @@
 export default (function view() {
 
     function insertProjectHeading(query, projectName) {
-        const contentDiv = document.querySelector(query);
+        const targetDiv = document.querySelector(query);
         const projectHeading = document.createElement('h2');
         projectHeading.textContent = projectName;
-        contentDiv.appendChild(projectHeading);
+        targetDiv.appendChild(projectHeading);
     }
 
     function insertProjectItem(query, textToUpdate) {
-        const contentDiv = document.querySelector(query);
+        const targetDiv = document.querySelector(query);
         const newItem = document.createElement('div');
         newItem.textContent = textToUpdate;
-        contentDiv.appendChild(newItem);
+        targetDiv.appendChild(newItem);
     }
 
     return {
