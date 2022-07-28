033c1f7b9e87330255245e66e263132e72d0c6de
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 14:23:27 +1000

    Change export to the todoItemFactory function

--- src/todo.js
+++ src/todo.js
@@ -32,7 +32,7 @@ const checklistMixin = {
 }
 
 // Responsibility: return/construct a todoItem object, which represents a single todo item
-const todoItemFactory = (itemTitle, itemDate='Today', itemDueDate=null, itemTags=[], itemNotes=null) => {
+export default function todoItemFactory (itemTitle, itemDate='Today', itemDueDate=null, itemTags=[], itemNotes=null) {
     const title = itemTitle;
     const date = itemDate;
     const dueDate = itemDueDate;
@@ -40,7 +40,19 @@ const todoItemFactory = (itemTitle, itemDate='Today', itemDueDate=null, itemTags
     const notes = itemNotes;
     const _checklist = [];
 
+    const printSummary = () => {
+        console.log(`
+            Title: ${title},
+            Date: ${date},
+            Due Date: ${dueDate},
+            Tags: ${tags},
+            Notes: ${notes}.
+            Checklist: ${_checklist}
+        `);
+    }
+
     const factoryMethods = {
+        printSummary
     }
 
     // copies "tag" properties into "item" properties
@@ -60,14 +72,18 @@ const todoItemFactory = (itemTitle, itemDate='Today', itemDueDate=null, itemTags
     return todoItemObj;
 }
 
+/*
 export default function loadTodoItems() {
     // const t1 = todoItemFactory('Brush Teeth', 'For oral hygiene', '01/01/2023', ['Hygiene']);
     const t1 = todoItemFactory('Brush Teeth');
-    const t2 = todoItemFactory('Take Shower', 'For overall hygiene', '02/01/2023', ['Entertainment']);
+    const t2 = todoItemFactory('Take Shower', 'Tomorrow', '02/01/2023', ['Entertainment'], 'For overall hygiene');
     t1.appendTag('Leisure');
-    t1.printTags();
+    // t1.printTags();
     t1.addTask('Buy new toothpaste');
-    t1.printChecklist();
+    // t1.printChecklist();
     t1.addTask('Buy new toothbrush');
     t1.printChecklist();
+    t1.printSummary();
+    // t2.printSummary();
 }
+*/
