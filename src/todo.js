e9ecc8c90b736e5390f79725e61a8c19f5aa27c7
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 13:07:39 +1000

    Move checklist methods into prototype object

--- src/todo.js
+++ src/todo.js
@@ -22,59 +22,56 @@ const tagProto = {
     }
 }
 
+const checkListProto = {
+    printChecklist () {
+        console.log(this.checklist);
+    },
+    addTask(task) {
+        this.checklist.push(task);
+    }
+}
+
 // Responsibility: return/construct a todoItem object, which represents a single todo item
 const todoItemFactory = (itemTitle, itemDescription=null, itemDueDate=null, itemTags=[]) => {
     const title = itemTitle;
     const description = itemDescription;
     const dueDate = itemDueDate;
     const tags = itemTags;
+    const _checklist = [];
 
     const oneMethod = () => console.log("Foo");
     const anotherMethod = () => console.log("Bar");
 
-    const _checklist = [];
-    const addTask = (task) => _checklist.push(task);
-    const printChecklist = () => console.log(_checklist);
-
     const factoryMethods = {
         oneMethod,
-        anotherMethod,
-        addTask,
-        printChecklist
+        anotherMethod
     }
 
     // copies "tag" properties into "item" properties
-    let proto = Object.assign(itemProto, tagProto);  // (targetObj, sourceObj)
+    let proto = Object.assign(itemProto, tagProto, checkListProto);  // (targetObj, sourceObj)
     // creates a new object, using an existing object as its prototype of newly created object
-    let obj = Object.create(proto);
+    let todoItemObj = Object.create(proto);
     // "mix-in" methods defined in factory function
-    obj = Object.assign(obj, factoryMethods);
+    todoItemObj = Object.assign(todoItemObj, factoryMethods);
 
-    obj.title = title;
-    obj.dueDate = dueDate;
-    obj.tags = tags;
-    // obj.checklist = checklist;
+    todoItemObj.title = title;
+    todoItemObj.dueDate = dueDate;
+    todoItemObj.tags = tags;
+    todoItemObj.checklist = _checklist;
 
-    return obj;
+    return todoItemObj;
 }
 
 export default function loadTodoItems() {
     // const t1 = todoItemFactory('Brush Teeth', 'For oral hygiene', '01/01/2023', ['Hygiene']);
     const t1 = todoItemFactory('Brush Teeth');
-    // const t2 = todoItemFactory('Take Shower', 'For overall hygiene', '02/01/2023', ['Entertainment']);
-    t1.describe();
-    t1.printTags();
+    const t2 = todoItemFactory('Take Shower', 'For overall hygiene', '02/01/2023', ['Entertainment']);
     t1.appendTag('Leisure');
     t1.printTags();
-    console.log(Object.getPrototypeOf(t1));
     t1.addTask('Buy new toothpaste');
     t1.printChecklist();
     t1.addTask('Buy new toothbrush');
     t1.printChecklist();
-    t2.addTask('Replace showerhead');
-    t2.printChecklist();
-    t2.describe();
-
     console.log("------------------");
 
     // for (let key in t1) {
