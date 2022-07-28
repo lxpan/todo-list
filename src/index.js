75f700de0203f99fea66d2ef44e550a27d39bf84
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 16:40:13 +1000

    Change setupHeading() to include all page setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import buildProject from './project.js';
 
 const run = () => {
     const defaultProject = buildProject();
+
+    function updateDOM() {
+
+    }
 }
 
-const createHeading  = () => {
+const setupHTML  = () => {
     const testHeading = document.createElement('h1');
+    const content = document.createElement('div');
+    content.id = 'content';
     testHeading.textContent = 'Todo List';
-    document.body.appendChild(testHeading);
+    
+    document.body.append(testHeading, content);
+
+
 }
 
-createHeading();
+setupHTML();
 run();
