682dcb534aba75f8ba9f4bb7c935ac7989043a17
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 14:47:35 +1000

    Get unique tags from project items

--- src/project.js
+++ src/project.js
@@ -20,19 +20,35 @@ class Project {
     set notes(text) {
         this._notes = text;
     }
+
+    getUniqueTags() {
+        const tags =  this.todoItems
+            .map(item => {
+                return item.tags[0];
+            });
+
+        const uniqueArray = Array.from(new Set(tags));
+        return uniqueArray;
+    }
 }
 
 export default function loadProject() {
     const p = new Project('Daily Tasks');
     console.log(p.name);
 
-    p.notes = 'My food journal.';
-    p.addItem('Drink water');
-    p.addItem('Eat pizza');
+    p.notes = 'My food journal';
+    
+    p.addItem('Mail parcel to Mum');
+    p.addItem('Pay the bills');
+    p.todoItems[0].appendTag('Errands');
+    p.todoItems[1].appendTag('Errands');
+    
+    p.addItem('Run for 10 minutes');
+    p.todoItems[2].appendTag('Fitness');
 
     p.todoItems.forEach(item => {
         item.printSummary();
     });
 
-    console.log(p.notes);
+    console.log(p.getUniqueTags());
 }
