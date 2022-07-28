d94ba89b101e907f41ac6ece01ebea916ceaa895
Luoxi Pan <l.panpax@gmail.com>
2022-07-28 14:29:19 +1000

    Create new items from Project class

--- src/project.js
+++ src/project.js
@@ -2,14 +2,28 @@ import todoItemFactory from './todo.js';
 
 class Project {
     notes;
-    items;
+    todoItems = [];
 
     constructor(name) {
         this.name = name;
+    }
+
+    addItem(title) {
+        const item = todoItemFactory(title);
+        this.todoItems.push(item);
+    }
+
+    get todoItems() {
+        return this._todoItems;
     }
 }
 
 export default function loadProject() {
     const p = new Project('Daily Tasks');
     console.log(p.name);
+
+    p.addItem('Drink water');
+    console.log(p.todoItems);
+    p.addItem('Eat pizza');
+    console.log(p.todoItems);
 }
