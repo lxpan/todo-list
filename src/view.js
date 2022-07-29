b632d6334001358b4c28c6d924226d2f67609eca
Luoxi Pan <l.panpax@gmail.com>
2022-07-29 16:25:24 +1000

    Add checkboxes to todoItems

--- src/view.js
+++ src/view.js
@@ -1,4 +1,5 @@
 export default (function view() {
+    let elementID = 0;
 
     function insertProjectHeading(query, projectName) {
         const targetDiv = document.querySelector(query);
@@ -9,9 +10,22 @@ export default (function view() {
 
     function insertProjectItem(query, textToUpdate) {
         const targetDiv = document.querySelector(query);
-        const newItem = document.createElement('div');
-        newItem.textContent = textToUpdate;
-        targetDiv.appendChild(newItem);
+        const itemDiv = document.createElement('div');
+        itemDiv.dataset.itemId = 'testID';
+
+        const input = document.createElement('input');
+        const label = document.createElement('label');
+
+        input.type = 'checkbox';
+        input.id = elementID; // link "id" and "for"
+        label.textContent = textToUpdate;
+        label.setAttribute('for', elementID); // link "id" and "for"
+
+        itemDiv.append(input, label);
+        targetDiv.appendChild(itemDiv);
+
+        // ensure each input-label pair has a unique integer identifier
+        elementID++;
     }
 
     return {
