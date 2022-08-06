520af2c36d044e24cec2374ddaed5c2f792b6223
Luoxi Pan <l.panpax@gmail.com>
2022-08-06 19:14:22 +1000

    Restructure index page with header and grid

--- src/index.js
+++ src/index.js
@@ -2,21 +2,32 @@ import buildProject from './project.js';
 import view from './view.js';
 import './style.css';
 
-
-const setupHTML  = () => {
+const setupHeader = () => {
+    const header = document.createElement('header');
     const testHeading = document.createElement('h1');
-    const content = document.createElement('div');
-    content.id = 'content';
     testHeading.textContent = 'Todo List';
-    
-    
 
     // "Create New Item" button
     const newItemBtn = document.createElement('button');
     newItemBtn.className = 'newItemBtn';
     newItemBtn.textContent = '+';
 
-    document.body.append(testHeading, newItemBtn, content);
+    header.append(testHeading, newItemBtn);
+    return header;
+}
+
+
+const setupHTML  = () => {
+    const gridContainer = document.createElement('div');
+    gridContainer.className = 'gridContainer';
+    const navbar = document.createElement('div');
+    
+    const content = document.createElement('div');
+    content.id = 'content';
+
+    gridContainer.append(navbar, content);
+
+    document.body.append(setupHeader(), gridContainer);
 }
 
 const run = () => {
