c519afd4f5d08278eeb99b6575ba8445feef1032
Luoxi Pan <l.panpax@gmail.com>
2022-08-12 16:11:09 +1000

    Rename loadMockItems function

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ function setupMockProject() {
     // console.log(defaultProject.todoItems);
 }
 
-const loadMockItems = () => {
+const loadMockItemsIntoDOM = () => {
     // enumerate items in default project
     Object.values(defaultProject.todoItems).forEach(item => {
         view.insertProjectItemForm('#content', item);
@@ -123,4 +123,4 @@ const defaultProject = buildProject();
 
 setupMockProject();
 setupHTML();
-loadMockItems();
+loadMockItemsIntoDOM();
