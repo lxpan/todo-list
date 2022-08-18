ed3144ec2a9d3636de8d7b56117ce3d404b26f4c
Luoxi Pan <l.panpax@gmail.com>
2022-08-18 16:50:14 +1000

    Refactor index.js functions into factory function

--- src/index.js
+++ src/index.js
@@ -6,131 +6,144 @@ import './style.css';
 const CONTENT_DIV_ID = 'content';
 const CONTENT_DIV_SELECTOR = `#${CONTENT_DIV_ID}`;
 
-const clickLastTodoItem = () => {
-    const items = document.querySelectorAll('.todoItem');
-    const last = items[items.length - 1];
-    
-    const lastElementClickable =
-        (last.getAttribute('titleType') == 'input')
-            ? last.querySelector("input[type='text']")
-            : last.querySelector('label');
-    
-    lastElementClickable.click();
-}
 
-const setNewItemBtn = () => {
-    const addNewItem = () => {
-        // add new item to project
-        defaultProject.addItem('');
+function projectRunner(projectName) {
+    const defaultProject = buildProject(projectName);
+
+    const clickLastTodoItem = () => {
+        const items = document.querySelectorAll('.todoItem');
+        const last = items[items.length - 1];
+        
+        const lastElementClickable =
+            (last.getAttribute('titleType') == 'input')
+                ? last.querySelector("input[type='text']")
+                : last.querySelector('label');
+        
+        lastElementClickable.click();
+    }
+    
+    const setNewItemBtn = () => {
+        const addNewItem = () => {
+            // add new item to project
+            defaultProject.addItem('');
+            // console.log(defaultProject.todoItems);
+    
+            // clear current items and reset div
+            const content = document.querySelector(CONTENT_DIV_SELECTOR);
+            content.innerHTML = '';
+            view.insertProjectHeading(CONTENT_DIV_SELECTOR, defaultProject.name);
+    
+            // repopulate list items from project
+            Object.values(defaultProject.todoItems).forEach(item => {
+                view.insertProjectItemForm(CONTENT_DIV_SELECTOR, item);
+                view.insertItemChangeListener(item.uuid, defaultProject);
+            });
+    
+            clickLastTodoItem();
+        }
+    
+        const newItemBtn = view.createButton('+', 'newItemBtn', addNewItem)
+        return newItemBtn;
+    }
+    
+    const setupDebugBtn = () => {
+        const logItemsInObject = () => {
+            console.table(defaultProject.todoItems, ['title', 'notes', 'date', 'dueDate', 'checklist', '_tags', 'completion', 'tagify']);
+        }
+    
+        const debugBtn = view.createButton('*', 'debugBtn', logItemsInObject);
+        return debugBtn;
+    }
+    
+    const setupHeader = () => {
+        const header = document.createElement('header');
+        const testHeading = document.createElement('h1');
+        testHeading.textContent = 'Todo List';
+    
+        header.append(testHeading, setNewItemBtn(), setupDebugBtn());
+        return header;
+    }
+    
+    function setupHTML() {
+        const gridContainer = view.createElement('div', 'gridContainer');
+        const navbar = view.createElement('div', 'navbar');
+    
+        const projects = ['Project 1', 'Project 2', 'Project 3'];
+        const projectList = document.createElement('ul');
+        
+        projects.forEach(project => {
+            const projectItem = document.createElement('li');
+            projectItem.textContent = project;
+            projectList.appendChild(projectItem);
+    
+        });
+        navbar.appendChild(projectList);
+    
+        const content = document.createElement('div');
+        content.id = CONTENT_DIV_ID;
+    
+        gridContainer.append(navbar, content);
+        document.body.append(setupHeader(), gridContainer);
+        
+        view.insertProjectHeading(`#${CONTENT_DIV_ID}`, defaultProject.name);
+    }
+    
+    function addMockTags(items) {
+        const todoItems = Object.values(items);
+        todoItems[0].addTagifyTag('Errands');
+        todoItems[1].addTagifyTag('Errands');
+        todoItems[2].addTagifyTag('Fitness');
+        todoItems[3].addTagifyTag(['Fitness', 'Study']);
+    }
+    
+    function setupMockProject() {
+        defaultProject.notes = 'My food journal';
+    
+        defaultProject.addItem('Mail parcel to Mum');
+        defaultProject.addItem('Pay the bills');
+        defaultProject.addItem('Run for 10 minutes');
+        defaultProject.addItem('Study on the exercise bike');
+    
+        const itemValues = Object.values(defaultProject.todoItems);
+    
+        itemValues[0].notes = 'Needs to be done at the post office.'
+        itemValues[0].addTask('Find item.');
+        itemValues[0].addTask('Pack parcel.');
+        itemValues[0].addTask('Mail parcel.');
+        // itemValues[0].setCompleted();
+        // itemValues[3].appendTag('Fitness', 'Study', 123);
+        // console.log(defaultProject.getUniqueTags());       
         // console.log(defaultProject.todoItems);
-
-        // clear current items and reset div
-        const content = document.querySelector(CONTENT_DIV_SELECTOR);
-        content.innerHTML = '';
-        view.insertProjectHeading(CONTENT_DIV_SELECTOR, defaultProject.name);
-
-        // repopulate list items from project
+    }
+    
+    const loadMockItemsIntoDOM = () => {
+        // only load from storage if key exists
+        if(localStorage.getItem(defaultProject.name)) {
+            defaultProject.retrieveLocalStorage();
+        }
+        
+        // enumerate items in default project
         Object.values(defaultProject.todoItems).forEach(item => {
-            view.insertProjectItemForm(CONTENT_DIV_SELECTOR, item);
+            view.insertProjectItemForm(`#${CONTENT_DIV_ID}`, item);
             view.insertItemChangeListener(item.uuid, defaultProject);
         });
-
-        clickLastTodoItem();
+    
+        addMockTags(defaultProject.todoItems);
+    
+        // view.tagifyAll(defaultProject);
     }
 
-    const newItemBtn = view.createButton('+', 'newItemBtn', addNewItem)
-    return newItemBtn;
-}
-
-const setupDebugBtn = () => {
-    const logItemsInObject = () => {
-        console.table(defaultProject.todoItems, ['title', 'notes', 'date', 'dueDate', 'checklist', '_tags', 'completion', 'tagify']);
+    const run = () => {
+        setupMockProject();
+        setupHTML();
+        loadMockItemsIntoDOM();
     }
 
-    const debugBtn = view.createButton('*', 'debugBtn', logItemsInObject);
-    return debugBtn;
-}
-
-const setupHeader = () => {
-    const header = document.createElement('header');
-    const testHeading = document.createElement('h1');
-    testHeading.textContent = 'Todo List';
-
-    header.append(testHeading, setNewItemBtn(), setupDebugBtn());
-    return header;
-}
-
-function setupHTML() {
-    const gridContainer = view.createElement('div', 'gridContainer');
-    const navbar = view.createElement('div', 'navbar');
-
-    const projects = ['Project 1', 'Project 2', 'Project 3'];
-    const projectList = document.createElement('ul');
-    
-    projects.forEach(project => {
-        const projectItem = document.createElement('li');
-        projectItem.textContent = project;
-        projectList.appendChild(projectItem);
-
-    });
-    navbar.appendChild(projectList);
-
-    const content = document.createElement('div');
-    content.id = CONTENT_DIV_ID;
-
-    gridContainer.append(navbar, content);
-    document.body.append(setupHeader(), gridContainer);
-    
-    view.insertProjectHeading(`#${CONTENT_DIV_ID}`, defaultProject.name);
-}
-
-function addMockTags(items) {
-    const todoItems = Object.values(items);
-    todoItems[0].addTagifyTag('Errands');
-    todoItems[1].addTagifyTag('Errands');
-    todoItems[2].addTagifyTag('Fitness');
-    todoItems[3].addTagifyTag(['Fitness', 'Study']);
-}
-
-function setupMockProject() {
-    defaultProject.notes = 'My food journal';
-
-    defaultProject.addItem('Mail parcel to Mum');
-    defaultProject.addItem('Pay the bills');
-    defaultProject.addItem('Run for 10 minutes');
-    defaultProject.addItem('Study on the exercise bike');
-
-    const itemValues = Object.values(defaultProject.todoItems);
-
-    itemValues[0].notes = 'Needs to be done at the post office.'
-    itemValues[0].addTask('Find item.');
-    itemValues[0].addTask('Pack parcel.');
-    itemValues[0].addTask('Mail parcel.');
-    // itemValues[0].setCompleted();
-    // itemValues[3].appendTag('Fitness', 'Study', 123);
-    // console.log(defaultProject.getUniqueTags());       
-    // console.log(defaultProject.todoItems);
-}
-
-const loadMockItemsIntoDOM = () => {
-    // only load from storage if key exists
-    if(localStorage.getItem(defaultProject.name)) {
-        defaultProject.retrieveLocalStorage();
+    return {
+        run,
+        defaultProject
     }
-    
-    // enumerate items in default project
-    Object.values(defaultProject.todoItems).forEach(item => {
-        view.insertProjectItemForm(`#${CONTENT_DIV_ID}`, item);
-        view.insertItemChangeListener(item.uuid, defaultProject);
-    });
-
-    addMockTags(defaultProject.todoItems);
-
-    // view.tagifyAll(defaultProject);
 }
 
-const defaultProject = buildProject('DailyTasks');
-
-setupMockProject();
-setupHTML();
-loadMockItemsIntoDOM();
+const firstProject = projectRunner('Superboo');
+firstProject.run();
